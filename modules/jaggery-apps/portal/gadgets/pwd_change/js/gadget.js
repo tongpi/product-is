b8d96dcace22c0bc736d96198aa9d8dd1506b72b
wangf <ten2net@gmail.com>
2019-05-10 11:48:18 +0800

    汉化dashboard修改密码页面

--- modules/jaggery-apps/portal/gadgets/pwd_change/js/gadget.js
+++ modules/jaggery-apps/portal/gadgets/pwd_change/js/gadget.js
@@ -5,24 +5,24 @@ function drawPage() {
         "              <table class=\"table table-bordered\">\n" +
         "                  <thead>\n" +
         "                      <tr>\n" +
-        "                          <th colspan=\"2\">Enter New Password</th>\n" +
+        "                          <th colspan=\"2\">新密码修改窗口</th>\n" +
         "                      </tr>\n" +
         "                  </thead>\n" +
         "                  <tbody>\n" +
         "                     <tr>\n" +
-        "                        <td><label class=\"\">Current Password</label> </td>\n" +
+        "                        <td><label class=\"\">当前密码</label> </td>\n" +
         "                        <td>\n" +
         "                            <input type=\"password\" value=\"\" id=\"currentPwd\" name=\"currentPwd\"  class=\"col-lg-3\" />\n" +
         "                        </td>\n" +
         "                     </tr>\n" +
         "                      <tr>\n" +
-        "                         <td><label class=\"\">New Password</label> </td>\n" +
+        "                         <td><label class=\"\">新密码</label> </td>\n" +
         "                         <td>\n" +
         "                             <input type=\"password\" value=\"\" id=\"newPwd\" name=\"newPwd\"  class=\"col-lg-3\" />\n" +
         "                         </td>\n" +
         "                      </tr>\n" +
         "                       <tr>\n" +
-        "                          <td><label class=\"\">Confirm New Password</label> </td>\n" +
+        "                          <td><label class=\"\">确认新密码</label> </td>\n" +
         "                          <td>\n" +
         "                              <input type=\"password\" value=\"\" id=\"confirmNewPwd\" name=\"confirmNewPwd\"  class=\"col-lg-3\" />\n" +
         "                          </td>\n" +
@@ -32,8 +32,8 @@ function drawPage() {
         "                </fieldset>\n" +
         "                <div class=\"control-group\">\n" +
         "                    <div class=\"controls\">\n" +
-        "                        <input type=\"button\" onclick=\"validate();\" class=\"btn btn-primary\" value=\"Update\"/>\n" +
-        "                        <input type=\"button\" onclick=\"cancel();\" class=\"btn btn-default btn-cancel\" value=\"Cancel\"/>\n" +
+        "                        <input type=\"button\" onclick=\"validate();\" class=\"btn btn-primary\" value=\"修改"/>\n" +
+        "                        <input type=\"button\" onclick=\"cancel();\" class=\"btn btn-default btn-cancel\" value=\"取消"/>\n" +
         "                    </div>\n" +
         "                </div>";
     output = start;
@@ -55,7 +55,7 @@ function validate() {
         "    <div class=\"modal-content\">\n" +
         "      <div class=\"modal-header\">\n" +
         "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>\n" +
-        "        <h3 class=\"modal-title\">Modal title</h4>\n" +
+        "        <h3 class=\"modal-title\">标题</h4>\n" +
         "      </div>\n" +
         "      <div class=\"modal-body\">\n" +
         "        <p>One fine body&hellip;</p>\n" +
